69ed2321d6302bbcf95a4c87997be2f0276a5d32
Zenul Aabedeen Khan <zaynaadi667@gmail.com>
2026-02-09 20:41:10 +0530

    Add MAGENTA color definition to sniffer3.cpp

--- sniffer3.cpp
+++ sniffer3.cpp
@@ -19,6 +19,7 @@ using namespace std;
 #define GREEN   "\033[32m"
 #define YELLOW  "\033[33m"
 #define BLUE    "\033[34m"
+#define MAGENTA    "\033[35m"
 #define CYAN    "\033[36m"
 #define BOLD    "\033[1m"
 
